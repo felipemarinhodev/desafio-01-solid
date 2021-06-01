0db565957c6a19c15e5c9840a4ea8b3b291e52a0
Felipe Marinho <felipmarinho@gmail.com>
2021-06-01 06:50:54 -0300

    configure swagger

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,15 @@
 import express from "express";
+import swaggerUi from "swagger-ui-express";
 
 import { usersRoutes } from "./routes/users.routes";
+// eslint-disable-next-line import/no-unresolved
+import swaggerFile from "./swagger.json";
 
 const app = express();
 
 app.use(express.json());
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use("/users", usersRoutes);
 
